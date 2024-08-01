f3872fc9bbcf08f2481c599b18597e5d7b00d083
Richard Kellnberger <git@richardk2n.de>
2024-08-01 15:17:43 +0200

    move setups

--- microgelAdhesionScalingTests.py
+++
@@ -1,9 +0,0 @@
-import Simulation as s
-import sys
-
-testSim = s.Simulation(sys.argv[1])
-testSim.setInteractionAdhesivePlaneNonDimensional()
-testSim.setInteractionNeoHookean()
-testSim.setInteractionPointZeroForce()
-testSim.setInteractionVelocityVerlet(fixTopBottom=True)
-testSim.run(writePressure=True, writeStress=True, writeVolumeChange=False)
