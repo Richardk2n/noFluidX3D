e339857fba71e8ed0d27c4fd45db98625bb2a143
Richard Kellnberger <git@richardk2n.de>
2025-05-15 16:02:23 +0200

    expose new interactions

--- nofluidx3d/interactions/__init__.py
+++ nofluidx3d/interactions/__init__.py
@@ -9,7 +9,10 @@ Created on Thu Mar 27 12:38:09 2025
 
 
 from nofluidx3d.interactions.MooneyRivlin import MooneyRivlin
+from nofluidx3d.interactions.Plane import Plane
 from nofluidx3d.interactions.PlaneAFM import PlaneAFM
+from nofluidx3d.interactions.Sphere import Sphere
+from nofluidx3d.interactions.Substrate import Substrate
 from nofluidx3d.interactions.VelocityVerlet import VelocityVerlet
 
-__all__ = ["MooneyRivlin", "PlaneAFM", "VelocityVerlet"]
+__all__ = ["MooneyRivlin", "Plane", "PlaneAFM", "Sphere", "Substrate", "VelocityVerlet"]
